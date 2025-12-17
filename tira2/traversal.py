e45a61d435063cf90361328b260a77d8b99a99b7
mersikka <mio@argillander.me>
2025-12-17 08:54:31 +0200

    Uhhhhhh

---
+++ tira2/traversal.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class TreeSet:
+    def __init__(self):
+        self.root = None
+
+    def add(self, value):
+        if not self.root:
+            self.root = Node(value)
+            return
+
+        node = self.root
+        while True:
+            if node.value == value:
+                return
+            if node.value > value:
+                if not node.left:
+                    node.left = Node(value)
+                    return
+                node = node.left
+            else:
+                if not node.right:
+                    node.right = Node(value)
+                    return
+                node = node.right
+
+    def __repr__(self):
+        if not self.root:
+            return []
+        res = []
+        seen = set()
+        queue = []
+        node = self.root
+        while node:
+            seen.add(node)
+            if node.left:
+                queue.append(node.left)
+        
+
+if __name__ == "__main__":
+    numbers = TreeSet()
+
+    numbers.add(4)
+    numbers.add(1)
+    numbers.add(2)
+    numbers.add(5)
+    numbers.add(8)
+    numbers.add(7)
+
+    print(numbers) # [1, 2, 4, 5, 7, 8]
